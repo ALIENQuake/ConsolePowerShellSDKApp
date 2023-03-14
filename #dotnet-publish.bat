6b503841290cbf0f02e73c8c123aa999a93b2fd6
ALIENQuake <alienquake@hotmail.com>
2023-03-14 11:21:28 +0100

    net8.0

--- #dotnet-publish.bat
+++ #dotnet-publish.bat
@@ -1,10 +1,10 @@
 chcp 65001
 
 SET Configuration=Release
-SET Framework=net7.0
+SET Framework=net8.0
 SET Runtime=win10-x64
-
-dotnet publish -c %Configuration% -f %Framework% -r %Runtime% /p:PublishSelfContained=true /p:RuntimeIdentifier=win10-x64 /p:TargetFramework=net7.0 /p:PublishSingleFile=True /p:IncludeAllContentForSelfExtract=True
+dotnet clean -c %Configuration% -f %Framework% -r %Runtime% 
+dotnet publish -v d -c %Configuration% -f %Framework% -r %Runtime% /p:PublishSelfContained=true /p:RuntimeIdentifier=win10-x64 /p:TargetFramework=net7.0 /p:PublishSingleFile=True /p:IncludeAllContentForSelfExtract=True
 
 @echo off
 if NOT ["%errorlevel%"]==["0"] PAUSE
